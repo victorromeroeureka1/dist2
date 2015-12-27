ebc0809fc178d93c683c08aaa553e42ed765dd63
victorromeroeureka1 <victor.romero.eureka@openmailbox.org>
2015-12-27 14:17:12 -0400

    firts
    firts

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/dinamic-dev'
+  },
+
+  seedDB: true
+};
